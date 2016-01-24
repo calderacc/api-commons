12ed21a2b1e84b3ad28e4c179f0c765314206890
Malte Hübner <maltehuebner@gmx.org>
2016-01-24 20:40:48 +0100

    Added connectionPool.js

---
+++ connectionPool.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var fs = require('fs');
+
+var configFile = fs.readFileSync('./config.json');
+var config;
+
+try
+{
+	config = JSON.parse(configFile);
+}
+catch (err)
+{
+	console.log('There has been an error parsing your JSON.')
+	console.log(err);
+}
+
+var mysqlConnectionPool = mysql.createPool({
+	host: config.database.host,
+	user: config.database.username,
+	password: config.database.password
+});
+
+exports.mysqlConnectionPool = mysqlConnectionPool;
